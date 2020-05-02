69830248f9352fd65510c5caaea0f6f479930422
Amarnaath Sivaraman <Amarnaath@gmail.com>
2020-05-03 00:39:51 +0530

    Initial Commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import math
+import requests
+
+print(sys.executable)
+r = requests.get("https://www.google.com")
+print(r.status_code)
+
+d = input("Your Name: ")
+test = "Test"
+print("Hello", d)
